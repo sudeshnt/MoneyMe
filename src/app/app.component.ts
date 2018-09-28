13bde91e0444efeefb2f68e492bca16f42adc650
Sudesh Nimesha <sudesh@calcey.com>
2018-09-28 12:41:08 +0530

    set up for FCM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,39 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ToastController } from 'ionic-angular';
+import { tap } from 'rxjs/operators';
 
 import { HomePage } from '../pages/home/home';
+
+import { FcmProvider } from "../providers/fcm/fcm";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, fcm: FcmProvider, toastCtrl: ToastController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // Get a FCM token
+      fcm.getToken();
+      // Listen to incoming messages
+      fcm.listenToNotifications().pipe(
+          tap(msg => {
+              // show a toast
+              const toast = toastCtrl.create({
+                  message: msg.body,
+                  duration: 3000
+              });
+              toast.present();
+          })
+      ).subscribe()
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Firebase } from '@ionic-native/firebase';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth } from 'angularfire2/auth';
 
@@ -10,6 +11,7 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 import { firebaseConfig } from './shared/config/firebase-config'
+import { FcmProvider } from '../providers/fcm/fcm';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import { firebaseConfig } from './shared/config/firebase-config'
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AngularFireAuth
+    AngularFireAuth,
+    Firebase,
+    FcmProvider
   ]
 })
 export class AppModule {}
